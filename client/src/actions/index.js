461bc8cf398e2dc216c709d1be776a62e8c83ee0
sonphan96 <phantruongson96@gmail.com>
2021-05-04 22:13:06 -0400

    API server

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,14 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './types';
 
 export const signIn = userId => {
   return {
@@ -12,3 +22,15 @@ export const signOut = () => {
     type: SIGN_OUT,
   };
 };
+
+export const createStream = formValues => async dispatch => {
+  const response = await streams.post('/streams', formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+// export const createStream = (formValues) => {
+//   return async (dispatch) => {
+//     streams.post('/streams', formValues)
+//   }
+// }
