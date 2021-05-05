06692a533b0681b9b1795c84507fba5754dd667f
sonphan96 <phantruongson96@gmail.com>
2021-05-04 23:54:49 -0400

    stream reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,6 @@ import {
   SIGN_IN,
   SIGN_OUT,
   CREATE_STREAM,
-  CREATE_STREAM,
   FETCH_STREAMS,
   FETCH_STREAM,
   DELETE_STREAM,
@@ -34,3 +33,27 @@ export const createStream = formValues => async dispatch => {
 //     streams.post('/streams', formValues)
 //   }
 // }
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
