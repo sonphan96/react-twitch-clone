85ef874e70fcc5ef743661e027a03cf666bdbae2
sonphan96 <phantruongson96@gmail.com>
2021-05-06 19:50:26 -0400

    modal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Modal from '../Modal';
+
 const StreamDelete = () => {
   return (
     <div>
-      StreamDelete
+      Stream Delete
+      <Modal />
     </div>
   );
 };
